42bf9f3c57da826be1762842ecd4660933c75074
Argenis Villasmil <argvil19@gmail.com>
2018-05-06 00:21:46 -0400

    changed port

--- functions/functions/helpers/make-config.js
+++ functions/functions/helpers/make-config.js
@@ -1,3 +1,3 @@
 module.exports = (mnKey, ip) => {
-    return 'rpcuser=GanAtiOPENdsLaTeOtIBEhesUpespOSI\nrpcpassword=toNTImpaNDYBrustoGrUTomEndrayAtW\nrpcallowip=127.0.0.1\nrpcbind=127.0.0.1\n#\nlisten=1\nserver=1\ndaemon=1\nmaxconnections=24\nport=8369\nmasternode=1\nmasternodeprivkey=' + mnKey + '\nexternalip=' + ip
+    return 'rpcuser=GanAtiOPENdsLaTeOtIBEhesUpespOSI\nrpcpassword=toNTImpaNDYBrustoGrUTomEndrayAtW\nrpcallowip=127.0.0.1\nrpcbind=127.0.0.1\n#\nlisten=1\nserver=1\ndaemon=1\nmaxconnections=24\nport=18369\nmasternode=1\nmasternodeprivkey=' + mnKey + '\nexternalip=' + ip
 }
