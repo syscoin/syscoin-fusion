ecc3fcc0d0128520d1b256376101a03276a2a773
argvil19 <argvil19@gmail.com>
2018-07-25 22:46:26 -0400

    main.js removed from .gitignore

---
+++ app/components/Wallet/Main.js
@@ -0,0 +1,92 @@
+// @flow
+import React, { Component } from 'react'
+import { Row, Col, Tabs } from 'antd'
+import Accounts from './Accounts'
+import Send from './Send'
+
+const Tab = Tabs.TabPane
+
+type Props = {
+};
+
+type State = {
+  aliases: Array<Object>,
+  address: string,
+  balance: string
+}
+
+export default class Wallet extends Component<Props, State> {
+  props: Props
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      aliases: [],
+      address: '',
+      balance: ''
+    }
+  }
+
+  componentWillMount() {
+    this.getAliases()
+    this.getCurrentAddress()
+    this.getCurrentBalance()
+  }
+
+  getCurrentAddress() {
+    this.props.currentSysAddress((err, address) => {
+      this.setState({
+        address
+      })
+    })
+  }
+
+  getCurrentBalance() {
+    this.props.currentBalance((err, balance) => {
+      this.setState({
+        balance
+      })
+    })
+  }
+
+  getAliases() {
+    this.props.getAliases((err, aliases) => {
+      this.setState({
+        aliases
+      })
+    })
+  }
+
+  generateCurrentAliasBalance() {
+    return (
+      <span style={{marginRight: 20}}>
+        Balance: {this.state.aliases.map(i => i.balance).reduce((prev, next) => (next + prev), 0)} SYS
+      </span>
+    )
+  }
+
+  render() {
+    return (
+      <Row>
+        <Col xs={24}>
+          <Tabs className='tabs-app' tabBarExtraContent={this.generateCurrentAliasBalance()}>
+            <Tab tab='Accounts' key='1'>
+              <Accounts
+                currentAliases={this.state.aliases}
+                currentBalance={this.state.balance}
+                currentAddress={this.state.address}
+                getTransactionsForAlias={this.props.getTransactionsForAlias}
+              />
+            </Tab>
+            <Tab tab='Send' key='2'>
+              <Send
+                currentAliases={this.state.aliases}
+              />
+            </Tab>
+          </Tabs>
+        </Col>
+      </Row>
+    )
+  }
+}
