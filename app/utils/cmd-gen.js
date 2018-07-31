cf009018bfdbf3c41ce416eb9a27e409f9a13db5
argvil19 <argvil19@gmail.com>
2018-07-30 21:06:08 -0700

    paths fix hoping the last

--- app/utils/cmd-gen.js
+++ app/utils/cmd-gen.js
@@ -12,28 +12,15 @@ const generateCmd = (type: string, cmdLine: string = ''): string => {
   const syscoinDataPath = getSysPath()
   let cmd = ''
 
-  if (OS === 'windows') {
-    switch (type) {
-      case 'syscoind':
-        cmd += `"${syscoindPath}" --datadir="${syscoinDataPath}" `
-        break
-      case 'cli':
-        cmd += `"${syscoinCliPath}" --datadir="${syscoinDataPath}" `
-        break
-      default:
-        return null
-    }
-  } else {
-    switch (type) {
-      case 'syscoind':
-        cmd += `${syscoindPath} --datadir="${syscoinDataPath}" `
-        break
-      case 'cli':
-        cmd += `${syscoinCliPath} --datadir="${syscoinDataPath}" `
-        break
-      default:
-        return null
-    }
+  switch (type) {
+    case 'syscoind':
+      cmd += `"${syscoindPath}" --datadir="${syscoinDataPath}" `
+      break
+    case 'cli':
+      cmd += `"${syscoinCliPath}" --datadir="${syscoinDataPath}" `
+      break
+    default:
+      return null
   }
 
   if (cmdLine.length) {
