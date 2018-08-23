1c39673c04d0072f0faee5b0a383b173b5b52d3a
argvil19 <argvil19@gmail.com>
2018-08-23 19:07:25 -0400

    add custom css

--- app/utils/startup.js
+++ app/utils/startup.js
@@ -1,5 +1,6 @@
 const { exec } = require('child_process')
 const fs = require('fs')
+const { join } = require('path')
 const swal = require('sweetalert')
 const { app } = require('electron').remote
 
@@ -36,6 +37,52 @@ const checkSyscoind = (dispatch, cb) => {
   })
 }
 
+const checkAndCreateDocFolder = () => {
+  // Get Doc's path
+  const appDocsPath = join(app.getPath('documents'), 'Fusion')
+  const cssTemplate = require('./helpers/css-custom-template')
+  const customCssPath = join(appDocsPath, 'custom.css')
+
+  if (!fs.existsSync(appDocsPath)) {
+    // If docs path doesnt exist, try to create it.
+    try {
+      fs.mkdirSync(appDocsPath)
+
+      // Attemps to copy custom files
+      fs.writeFileSync(
+        join(appDocsPath, 'custom.css'),
+        cssTemplate
+      )
+    } catch (e) {
+      // Show an alert if an error comes up
+      return swal('Error', 'There was an error while reading App\'s document folder', 'error')
+    }
+  }
+
+  if (!fs.existsSync(customCssPath)) {
+    // If cant find custom.css file, regenerate it.
+    fs.writeFileSync(
+      join(appDocsPath, 'custom.css'),
+      cssTemplate
+    )
+  }
+}
+
+const loadCustomCss = () => {
+  const customCssPath = join(app.getPath('documents'), 'Fusion', 'custom.css')
+
+  try {
+    const css = fs.readFileSync(customCssPath)
+
+    const style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = css;
+    document.body.appendChild(style);
+  } catch(e) {
+    swal('Error', 'Error while loading custom CSS', 'error')
+  }
+}
+
 const startUpRoutine = (dispatch, env) => {
   if (!fs.existsSync(getSysPath('default'))) {
     // Attemps to create SyscoinCore folder if this doesn't exists already.
@@ -50,6 +97,10 @@ const startUpRoutine = (dispatch, env) => {
   
   // Sets the datadir in reducer
   dispatch(changeSyscoinDataDir('default'))
+
+  // Docs path initialization
+  checkAndCreateDocFolder()
+  loadCustomCss()
 
   // Executes syscoind (just in case it's not running already). It'll fail gracefully if its already running
   exec(generateCmd('syscoind', ''), (err) => {
