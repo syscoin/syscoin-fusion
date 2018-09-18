31cf6a8c09b76addff3427f8c8c29562cf01dbbe
lil-tree <quangogster@gmail.com>
2018-09-17 21:12:13 -0700

    tracking with count

--- functions/endpoints/data-tracking.js
+++ functions/endpoints/data-tracking.js
@@ -1,38 +1,63 @@
 const admin = require('firebase-admin')
 
 module.exports = (req, res, next) => {
-    const {
-        tag
-    } = req.body
+    const {tag} = req.body;
 
-    if (!tag) {
+    if (!isTagValid(tag)) {
         return res.status(422).json({
             error: true,
-            message: 'Lacking required parameters.'
+            message: 'invalid tag value'
         })
     }
 
-    if(tag === ''){
-        return res.status(422).json({
-            error: true,
-            message: 'tags required.'
-        })
-    }
+    admin.database().ref('/data-tracking')
+        .once('value', snapshot => {
+            let collectionObj = snapshot.val()
+            if (!collectionObj[tag]) {
+                collectionObj[tag] = 1;
+                writeToDataTracking(collectionObj)
+            } else {
+                let currentValue = collectionObj[tag]
+                collectionObj[tag] = currentValue + 1;
+                writeToDataTracking(collectionObj)
+            }
+        }).catch(() => res.status(500).send({
+        error: true,
+        message: 'Internal Server Error.'
+    }))
 
-    admin.database().ref('/data-tracking').set({
-        tag: tag
-    }, function(error) {
-        if (error) {
-            res.status(500).json({
-                error: true,
-                message: 'Internal server error' + error
-            })
-        } else {
-            res.status(200).json({
-                error: false,
-                message: 'successfully saved'
-            })
+    function isTagValid(tag) {
+        const regex = /^[A-Za-z]+$/
+
+        if (tag && !tag.match(regex)) {
+            return false;
         }
-    });
+
+        if (!tag) {
+            return false;
+        }
+
+        if (tag === '') {
+            return false;
+        }
+
+        return true;
+    }
+
+    function writeToDataTracking(dataObj) {
+        admin.database().ref('/data-tracking').set(dataObj, function (error) {
+            if (error) {
+                res.status(500).json({
+                    error: true,
+                    message: 'Internal server error' + error
+                })
+            } else {
+                res.status(200).json({
+                    error: false,
+                    message: 'successfully saved'
+                })
+            }
+        });
+    }
 }
 
