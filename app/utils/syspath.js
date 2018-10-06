8aea8b22a23158035fa365262cb14c290f9d21ea
argvil19 <argvil19@gmail.com>
2018-10-05 20:42:40 -0700

    fix sys path in mac

--- app/utils/syspath.js
+++ app/utils/syspath.js
@@ -1,4 +1,4 @@
-const appData = process.env.APPDATA || (process.platform === 'darwin' ? `${process.env.HOME}Library/Preferences` : '/var/local')
+const appData = process.env.APPDATA || (process.platform === 'darwin' ? `${process.env.HOME}/Library/Application Support` : '/var/local')
 const path = require('path')
 const OS = require('./detect-os')()
 
