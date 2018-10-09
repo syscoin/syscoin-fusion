0f2e461a7c8575933f6cf4889cc0f485361e6887
Ramyya Hari <ramyya@gmail.com>
2018-10-09 07:23:07 -0700

    Changed coinbase postback for wallet

--- functions/endpoints/coinbase-postback.js
+++ functions/endpoints/coinbase-postback.js
@@ -2,7 +2,7 @@ const firebase = require('firebase-functions')
 const admin = require('firebase-admin')
 var _ = require('lodash');
 
-const updateExpiry = require('./helpers/update-expiry')
+const updateBalance = require('./helpers/update-balance')
 
 /**
  * @api {post} /coinbase-postback Coinbase postback
@@ -17,50 +17,16 @@ module.exports = (req, res) => {
     if (typeof  _.findKey(data.timeline, {status: 'COMPLETED'}) !== 'undefined') {
     	console.log('Charge confirmed: ', data.code)
 
-        if (data.metadata.renew) {
-            const payload = data.metadata
-            payload.paymentMethod = payload.method
-
-            updateExpiry(payload, (err, data) => {
-                if (err) {
-                    return res.status(500).send({
-                        error: true,
-                        message: 'Error'
-                    })    
-                }
-
-                return res.status(200).send({
-                    message: 'Success'
-                })
-            })
-        } else {
-        	const months = data.metadata.months,
-                email = data.metadata.email,
-                mnKey = data.metadata.mnKey,
-                mnTxid = data.metadata.mnTxid,
-                mnName = data.metadata.mnName,
-                mnIndex = data.metadata.mnIndex,
-                userId =  data.metadata.userId,
-                paymentMethod = data.metadata.method,
-                nodeType = req.body.nodeType || 'sys'
-
-            admin.database().ref('/to-deploy/tasks').push({
-                months,
-                mnKey,
-                mnTxid,
-                mnName,
-                mnIndex,
-                paymentMethod,
-                lock: false,
-                lockDate: null,
-                orderDate: data.created_at,
-                paymentId: data.code,
-                deployed: false,
-                userId,
-                nodeType
-            })
-            return res.send().status(200);
-        }
         
+        updateBalance(data.metadata.uid, data.metadata.chargeAmount, (err) => {
+            if (err) {
+                return res.status(400).send({
+                    error: true,
+                    message: 'Error updating balance. Contact support'
+                })
+            }
+        });  
+
+        return res.send().status(200);
     } 
 }
