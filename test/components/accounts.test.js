8df1c42b38e73def8d5bb775c3ef8a7a4cedc9ce
argvil19 <argvil19@gmail.com>
2018-11-13 18:40:37 -0400

    fixed Accounts component test. Adding a few more WIP

--- test/components/accounts.test.js
+++ test/components/accounts.test.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Accounts from 'fw-components/Accounts'
+import SyncLoader from 'fw-components/Accounts/components/sync-loader'
+import Dashboard from 'fw-components/Accounts/components/dashboard'
 import Enzyme, { shallow, render } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import { spy } from 'sinon'
@@ -7,67 +9,81 @@ import { spy } from 'sinon'
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('Accounts component tests', () => {
-    let wrapper
-    let props
+  let wrapper
+  let props
 
-    beforeEach(() => {
-        props = {
-            backgroundLogo: '',
-            balance: 123,
-            aliases: [],
-            transactions: {
-                data: [],
-                error: false,
-                isLoading: false
-            },
-            selectedAlias: '',
-            aliasAssets: {
-                data: [],
-                error: false,
-                isLoading: false
-            },
-            updateSelectedAlias: spy(),
-            selectAsset: spy(),
-            headBlock: 9999,
-            currentBlock: 500,
-            syncPercentage: parseInt((500 / 9999) * 100, 10)
-        }
-        wrapper = shallow(<Accounts {...props} />)
-    })
+  beforeEach(() => {
+    props = {
+      backgroundLogo: '',
+      balance: 123,
+      aliases: [],
+      transactions: {
+        isLoading: false,
+        data: [],
+        error: false
+      },
+      dashboardSysTransactions: {
+        data: [],
+        error: false,
+        isLoading: false
+      },
+      dashboardAssets: {
+        isLoading: false,
+        error: false,
+        data: []
+      },
+      selectedAlias: '',
+      aliasAssets: {
+        data: [],
+        error: false,
+        isLoading: false
+      },
+      updateSelectedAlias: spy(),
+      selectAsset: spy(),
+      headBlock: 9999,
+      currentBlock: 500,
+      syncPercentage: parseInt((500 / 9999) * 100, 10),
+      getPrivateKey: spy(),
+      goToHome: spy(),
+      getDashboardAssets: spy(),
+      getDashboardTransactions: spy(),
+      goToAssetForm: spy(),
+      goToSysForm: spy()
+    }
+    wrapper = shallow(<Accounts {...props} />)
+  })
 
-    it('should render successfully', () => {
-        expect(wrapper.find('.accounts-container').length).toEqual(1)
-    })
+  it('should render successfully', () => {
+    expect(wrapper.find('.accounts-container').length).toEqual(1)
+  })
 
-    it('should render coin logo if there is no selectedAlias/error', () => {
-        expect(wrapper.find('img.sys-logo-bg').length).toEqual(1)
-    })
+  it('should render dashboard if there is no selectedAlias/error', () => {
+    expect(wrapper.find(Dashboard).length).toEqual(1)
+  })
 
-    it('should not render coin logo if an alias is selected', () => {
-        props.selectedAlias = 'test'
-        wrapper = shallow(<Accounts {...props} />)
+  it('should not render coin logo if an alias is selected', () => {
+    props.selectedAlias = 'test'
+    wrapper = shallow(<Accounts {...props} />)
 
-        expect(wrapper.find('img.sys-logo-bg').length).toEqual(0)
-    })
+    expect(wrapper.find(Dashboard).length).toEqual(0)
+  })
 
-    it('should render coin logo if an error happens while selecting alias', () => {
-        props.selectedAlias = 'test'
-        props.aliasAssets.error = true
-        wrapper = shallow(<Accounts {...props} />)
+  it('should render Dashboard if an error happens while selecting alias', () => {
+    props.aliasAssets.error = true
+    wrapper = shallow(<Accounts {...props} />)
 
-        expect(wrapper.find('img.sys-logo-bg').length).toEqual(1)
-    })
+    expect(wrapper.find(Dashboard).length).toEqual(1)
+  })
 
-    it('should render loader if syncPercentage is not 100%', () => {
-        wrapper = render(<Accounts {...props} />)
-        expect(wrapper.find('div.sync-loader').length).toEqual(1)
-    })
+  it('should render loader if syncPercentage is not 100%', () => {
+    expect(wrapper.find(SyncLoader).length).toEqual(1)
+  })
 
-    it('should not render loader if syncPercentage is 100%', () => {
-        props.currentBlock = 9999
-        props.syncPercentage = parseInt((props.currentBlock / props.headBlock) * 100, 10)
-        wrapper = render(<Accounts {...props} />)
+  it('should not render loader if syncPercentage is 100%', () => {
+    props.currentBlock = 9999
+    props.syncPercentage = parseInt((props.currentBlock / props.headBlock) * 100, 10)
+    wrapper = render(<Accounts {...props} />)
 
-        expect(wrapper.find('div.sync-loader').length).toEqual(0)
-    })
+    expect(wrapper.find(SyncLoader).length).toEqual(0)
+  })
 })
--- test/reducers/options.test.js
+++ test/reducers/options.test.js
@@ -2,34 +2,34 @@ import reducer, { initialState } from 'fw-reducers/options'
 import * as types from 'fw-types/options'
 
 describe('Options reducer', () => {
-    it('should return initial state', () => {
-        const action = {}
-        expect(reducer(initialState, action)).toEqual({...initialState})
-    })
+  it('should return initial state', () => {
+    const action = {}
+    expect(reducer(initialState, action)).toEqual({ ...initialState })
+  })
 
-    it('should handle ALLOWED_GUIDS', () => {
-        const action = {
-            type: types.ALLOWED_GUIDS,
-            payload: ['0c9df9a04d306e02']
-        }
-        const expectedState = {
-            ...initialState,
-            guids: action.payload
-        }
+  it('should handle ALLOWED_GUIDS', () => {
+    const action = {
+      type: types.ALLOWED_GUIDS,
+      payload: ['0c9df9a04d306e02']
+    }
+    const expectedState = {
+      ...initialState,
+      guids: action.payload
+    }
 
-        expect(reducer(initialState, action)).toEqual(expectedState)
-    })
+    expect(reducer(initialState, action)).toEqual(expectedState)
+  })
 
-    it('should handle TOGGLE_MAXIMIZE', () => {
-        const action = {
-            type: types.TOGGLE_MAXIMIZE,
-            payload: true
-        }
-        const expectedState = {
-            ...initialState,
-            isMaximized: true
-        }
+  it('should handle TOGGLE_MAXIMIZE', () => {
+    const action = {
+      type: types.TOGGLE_MAXIMIZE,
+      payload: true
+    }
+    const expectedState = {
+      ...initialState,
+      isMaximized: true
+    }
 
-        expect(reducer(initialState, action)).toEqual(expectedState)
-    })
+    expect(reducer(initialState, action)).toEqual(expectedState)
+  })
 })
