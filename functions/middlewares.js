45f403b6b166c34d58dc7716fe3672ebfe7cd31a
Argenis Villasmil <argvil19@gmail.com>
2018-11-18 22:15:44 -0400

    changed daily charge contional

--- functions/middlewares.js
+++ functions/middlewares.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin')
 const updateBalance = require('./endpoints/helpers/update-balance')
-const updateLastUpdated = require('./endpoints/helpers/edit-mn-data')
+const updateLastUpdated = require('./endpoints/helpers/edit-last-charge')
 
 module.exports.checkIpWhitelist = (req, res, next) => {
     const clientIp = (req.headers['x-forwarded-for'] ||
@@ -8,10 +8,10 @@ module.exports.checkIpWhitelist = (req, res, next) => {
         req.socket.remoteAddress ||
         req.connection.socket.remoteAddress).split(",")[0]
 
-        admin.database().ref('/vps')
-            .orderByChild('ip')
-            .equalTo(clientIp)
-            .once('value', snapshot => {
+    admin.database().ref('/vps')
+        .orderByChild('ip')
+        .equalTo(clientIp)
+        .once('value', snapshot => {
             if (snapshot.hasChildren()) {
                 return next()
             } else {
@@ -20,7 +20,7 @@ module.exports.checkIpWhitelist = (req, res, next) => {
                     message: 'Forbidden'
                 })
             }
-    
+
         }).catch(() => res.sendStatus(500))
 }
 
@@ -36,7 +36,7 @@ module.exports.checkIpForUpdate = (req, res, next) => {
             if (snapshot.hasChildren()) {
                 const key = Object.keys(snapshot.val())[0]
                 const data = snapshot.val()[key]
-                
+
                 req.mnUserId = data.userId;
                 req.orderId = data.orderId;
 
@@ -52,13 +52,13 @@ module.exports.checkIpForUpdate = (req, res, next) => {
 
 module.exports.getMNType = (req, res, next) => {
     admin.database().ref('/mn-data')
-            .orderByChild('orderId')
-            .equalTo(req.orderId)
-            .once('value', snapshot => {
+        .orderByChild('orderId')
+        .equalTo(req.orderId)
+        .once('value', snapshot => {
             if (snapshot.hasChildren()) {
                 const key = Object.keys(snapshot.val())[0]
                 const data = snapshot.val()[key]
-                
+
                 req.mnType = data.nodeType;
                 req.chargeLastMadeAt = data.chargeLastMadeAt
 
@@ -73,34 +73,32 @@ module.exports.getMNType = (req, res, next) => {
 }
 
 module.exports.getOrderData = (req, res, next) => {
-    var now = new Date();
-    var startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-    var timestamp = startOfDay / 1000;
-    
-    // Check if update was done in the last day
-    if(parseInt(req.chargeLastMadeAt) > timestamp &&  parseInt(req.chargeLastMadeAt) < (timestamp + 24*60*60)) {
+    const nextChargeDate = req.chargeLastMadeAt + (24 * 60 * 60)
+
+    // Check if should charge based on last charge date
+    if (nextChargeDate > Date.now()) {
         return next()
     } else {
-        admin.database().ref('/prices/'+req.mnType)
-        .once('value', snapshot => {
-            const amount = snapshot.val()
+        admin.database().ref('/prices/' + req.mnType)
+            .once('value', snapshot => {
+                const amount = snapshot.val()
 
-            updateBalance(req.mnUserId, parseFloat(amount)*-1,
-                (err) => {
-                if (err) {
-                    console.log("Error in update balance: ", err)
-                    // res.sendStatus(500)
-                }
+                updateBalance(req.mnUserId, parseFloat(amount) * -1,
+                    (err) => {
+                        if (err) {
+                            console.log("Error in update balance: ", err)
+                            // res.sendStatus(500)
+                        }
 
-                updateLastUpdated(req.orderId, (err) => {
-                    if (err) {
-                        console.log("Error in update mn charge last updated: ", err)
-                        // res.sendStatus(500)
-                    }
-                })
-            })
+                        updateLastUpdated(req.orderId, (err) => {
+                            if (err) {
+                                console.log("Error in update mn charge last updated: ", err)
+                                // res.sendStatus(500)
+                            }
+                        })
+                    })
 
-            return next() 
-        }).catch(() => res.sendStatus(500))
-    } 
+                return next()
+            }).catch(() => res.sendStatus(500))
+    }
 }
