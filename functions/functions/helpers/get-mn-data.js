e9d631c52f5a079e283fd71ec258c500b13484ac
Argenis Villasmil <argvil19@gmail.com>
2018-11-26 23:06:30 -0400

    increased wallet balance watch cron time. Added fallback tto get-mn-data. Fixes to time calculation in get-order-data

--- functions/functions/helpers/get-mn-data.js
+++ functions/functions/helpers/get-mn-data.js
@@ -13,6 +13,8 @@ module.exports = (mnId, cb) => {
         }
         return cb(info)
       }
+
+      return cb('NOT_FOUND')
     })
 }
   
--- functions/functions/helpers/get-order-data.js
+++ functions/functions/helpers/get-order-data.js
@@ -2,53 +2,52 @@ const admin = require('firebase-admin')
 
 const getMNData = require('../helpers/get-mn-data')
 
-module.exports = (uid,cb) => {
-    let list = []
-    let index = 0, length
-  
-    admin.database().ref('/orders')
+module.exports = (uid, cb) => {
+  let list = []
+  let index = 0, length
+
+  admin.database().ref('/orders')
     .orderByChild('userId')
     .equalTo(uid)
     .once('value', snapshot => {
-      
-      length = snapshot.numChildren() 
+
+      length = snapshot.numChildren()
       if (snapshot.hasChildren()) {
-        while(true) {
+        while (true) {
           const key = Object.keys(snapshot.val())[index]
           const data = snapshot.val()[key]
-  
+
           if (!data) {
             break
           }
-  
+
           let daysLeft
-  
-          if (data.expiresOn < Date.now() + (24 * 60 * 60)) {
+
+          if (data.expiresOn < Date.now() + (24 * 60 * 60 * 1000)) {
             daysLeft = 1
-          } else if ((data.expiresOn > Date.now() + (24 * 60 * 60* 1)) && (data.expiresOn < Date.now() + (24 * 60 * 60* 3))) {
+          } else if ((data.expiresOn > Date.now() + (24 * 60 * 60 * 1000)) && (data.expiresOn < Date.now() + (24 * 60 * 60 * 1000 * 3))) {
             daysLeft = 3
-          } else if ((data.expiresOn > Date.now() + (24 * 60 * 60* 3)) && (data.expiresOn < Date.now() + (24 * 60 * 60* 5))) {
+          } else if ((data.expiresOn > Date.now() + (24 * 60 * 60 * 1000 * 3)) && (data.expiresOn < Date.now() + (24 * 60 * 60 * 1000 * 5))) {
             daysLeft = 5
-          } else if ((data.expiresOn > Date.now() + (24 * 60 * 60* 5)) && (data.expiresOn < Date.now() + (24 * 60 * 60* 7))) {
+          } else if ((data.expiresOn > Date.now() + (24 * 60 * 60 * 1000 * 5)) && (data.expiresOn < Date.now() + (24 * 60 * 60 * 1000 * 7))) {
             daysLeft = 7
           } else {
             daysLeft = 0
           }
-  
-          if(daysLeft) {
-            getMNData(data.mnDataId, (listData) => 
-            {
+
+          if (daysLeft) {
+            getMNData(data.mnDataId, (listData) => {
               list.push(listData)
-              if (list.length === length) 
+              if (list.length === length)
                 return cb(list)
             })
           } else {
             continue
           }
-  
+
           index++
         }
-        
+
       }
     })
 }
