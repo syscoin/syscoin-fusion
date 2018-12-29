aea9be9863d4801461a4a7f578f8bdea29cf2cb8
Argenis Villasmil <argvil19@gmail.com>
2018-12-29 18:17:36 -0400

    fixes wrong require statement

--- functions/functions/helpers/aws/delete-node.js
+++ functions/functions/helpers/aws/delete-node.js
@@ -1,5 +1,5 @@
 const AWS = require('./init')
-const terminateInstance = require('terminate-instance')
+const terminateInstance = require('./terminate-instance')
 const detachIp = require('./detach-ip')
 
 module.exports = (instanceId, allocationId) => new Promise(async (resolve, reject) => {
